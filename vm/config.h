f49e95cd1adc9d1a5773606f87cc86399e131af5
macournoyer <macournoyer@gmail.com>
2009-02-01 20:41:25 -0500

    Renaming stuff

--- vm/config.h
+++ vm/config.h
@@ -1,8 +1,8 @@
 /* Direct threaded code is used to dispatch instructions.
    It's very fast, but GCC specific. */
 #if __GNUC__ > 3
-#define MIN_THREADED_DISPATCH 1
+#define TR_THREADED_DISPATCH 1
 #endif
 
 /* max number of stack frame in the VM */
-#define MIN_MAX_FRAME 256
+#define TR_MAX_FRAME 256
--- vm/tr.h
+++ vm/tr.h
@@ -1,6 +1,46 @@
 #ifndef _TINYRB_H_
 #define _TINYRB_H_
 
+#include <stdlib.h>
+#include <limits.h>
+#include "kvec.h"
+#include "config.h"
 
+#define TR_ALLOC(T)          (T *)malloc(sizeof(T))
+#define TR_ALLOC_N(T,N)      (T *)malloc(sizeof(T)*(N))
+
+#define TR_MEMZERO(X,T)      memset((X), 0, sizeof(T))
+#define TR_MEMZERO_N(X,T,N)  memset((X), 0, sizeof(T)*(N))
+#define TR_MEMCPY(X,Y,T)     memcpy((X), (Y), sizeof(T))
+#define TR_MEMCPY_N(X,Y,T,N) memcpy((X), (Y), sizeof(T)*(N))
+
+#define VM                   TrVM *vm
+#define FRAME                &vm->frame[vm->cf]
+
+typedef unsigned long OBJ;
+
+enum TR_T {
+  TR_T_OBJECT, TR_T_CLASS, TR_T_SYMBOL, TR_T_STRING,
+  TR_T_MAX /* keep last */
+};
+
+typedef struct {
+  kvec_t(OBJ) stack;
+  char *fn;
+  int line; /* cur line num */
+  OBJ self;
+} TrFrame;
+
+typedef struct {
+  size_t cf; /* current frame */
+  TrFrame frames[TR_MAX_FRAME];
+} TrVM;
+
+typedef unsigned char TrOpCode;
+typedef struct {
+  kvec_t(TrOpCode) ops;
+  kvec_t(OBJ) lit;
+  char *fn;
+} TrCode;
 
 #endif /* _TINYRB_H_ */
